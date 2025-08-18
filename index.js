a1519f08aa1866ab6d7facfbe9d1b6cb63608e1b
Nguyễn Thanh Tuấn <104611730+Tuanlor207@users.noreply.github.com>
2025-08-18 17:19:30 +0700

    Update index.js
    hahaha

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
-//twan
+//twan bnjj
 const app = express();
 app.use("/", createProxyMiddleware({
   target: "https://discord.com",
